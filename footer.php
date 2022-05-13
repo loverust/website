06f26c63dd40aa65f33b3737449d9629c3298425
loverust <rust.github@gmail.com>
2022-05-13 22:56:41 +0700

    implement

---
+++ footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Theme Footer
+ *
+ * @package Kent
+ */
+
+	get_sidebar();
+?>
+		<footer role="contentinfo" id="footer">
+<?php
+	if ( function_exists( 'the_privacy_policy_link' ) ) {
+		the_privacy_policy_link( '', '<span class="sep"> | </span>' );
+	}
+?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'kent' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'kent' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'kent' ), 'Kent', '<a href="https://prothemedesign.com/" rel="designer">Pro Theme Design</a>' ); ?>
+		</footer>
+	</div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
